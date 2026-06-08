3447f35086b36416ff4b38b43c393ce1890cbdba
Navya S Shetty <shettynavya328@gmail.com>
2026-06-08 16:06:56 +0530

    Restored SQLite and fixed mobile responsive layout

--- Foodigo/settings.py
+++ Foodigo/settings.py
@@ -73,16 +73,11 @@ WSGI_APPLICATION = 'Foodigo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
-import os
-
-import dj_database_url
-import os
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
